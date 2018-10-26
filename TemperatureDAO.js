56739e32774805674eda4eb0361be0416beb7c8f
Tintou <martin.lm@outlook.fr>
2018-10-25 21:17:04 -0400

    Test unitaire lister et chercher Temperature

--- TemperatureDAO.js
+++ TemperatureDAO.js
@@ -1,17 +1,39 @@
 var postgresql = require('pg');
-var chaineDeConnection = 'postgres://postgres:test@localhost:5432/meteo';
+var chaineDeConnection = 'postgres://postgres:root@localhost:5432/meteo';
 
 exports.listerTemperature = async function()
 {
+    console.log("TemperatureDAO.listerTemperature()");
+    var basededonnees = new postgresql.Client(chaineDeConnection);
+    await basededonnees.connect();
+    var curseurListeTemperature = await basededonnees.query('select * from capteurtemperature');
 
+    var listeTemperature = {}; var position = 0;
+    curseurListeTemperature.rows.forEach
+        (
+              CapteurTemperature=>
+              {
+                    console.log("DAO:" + CapteurTemperature.temperature);
+                    listeTemperature[position++] = CapteurTemperature;
+              }
+        );
+
+        return listeTemperature;
 }
 
-exports.chercherTemperature = async function(temperature)
+exports.chercherTemperature = async function(numero)
 {
-
+    console.log("TemperatureDAO.chercherTemperature()");
+    var basededonnees = new postgresql.Client(chaineDeConnection);
+	  await basededonnees.connect();
+    var sql = 'select * from capteurtemperature where id = ' + numero;
+	  console.log(sql);
+    var curseurListeTemperature = await basededonnees.query(sql);
+	  var temperature = curseurListeTemperature.rows[0];
+	  return temperature;
 }
 
 exports.ajouterTemperature = async function(temperature)
 {
-
+    console.log("TemperatureDAO.ajouterTemperature()");
 }
--- service.js
+++ service.js
@@ -1,11 +1,25 @@
-var meteoDAO = require('./TemperatureDAO');
+var temperatureDAO = require('./TemperatureDAO');
 var http = require('http');
 
 var repondre = async function(requete, reponse)
 {
     if('GET' === requete.method)
 	  {
+        if('/CapteurTemperature/liste' === requete.url || '/CapteurTemperature/liste/' === requete.url)
+		    {
+          var listeTemperature = await temperatureDAO.listerTemperature();
+			    reponse.end(JSON.stringify(listeTemperature));
+        }
 
+        if(trouvailles = requete.url.match(/\/CapteurTemperature\/([0-9]+)\/?/))
+		    {
+			       numero = trouvailles[1];
+			       console.log('match une seule temperature ' + numero);
+
+			       var temperature = await TemperatureDAO.chercherTemperature(numero);
+			       console.log('temperature reçu de la base ' + JSON.stringify(temperature));
+			       reponse.end(JSON.stringify(temperature));
+		     }
     }
 
     if('POST' === requete.method)
