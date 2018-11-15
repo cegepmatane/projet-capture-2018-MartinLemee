3186c841783fafea1a256edfe1e584fc650dcf2a
ValBra <valere.brable8@etu.univ-lorraine.fr>
2018-11-15 09:19:10 -0500

    Package donnees

---
+++ JavaFX/src/donnees/Acces.java
@@ -0,0 +1,8 @@
+package donnees;
+
+public class Acces {
+	static final String BASEDEDONNEES_DRIVER = "";
+	static final String BASEDEDONNEES_URL = "";
+	static final String BASEDEDONNEES_USAGER = "";
+	static final String BASEDEDONNEES_MOTDEPASSE = "";	
+}
