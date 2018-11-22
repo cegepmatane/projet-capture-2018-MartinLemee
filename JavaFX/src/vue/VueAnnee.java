c754568df1391a609cf6fa97cd5a7534679e05c6
AlexandreCM <alexandre.mezeray@gmail.com>
2018-11-21 21:44:47 -0500

    lecture du fichier xml via url + recuperation des donnees

--- JavaFX/src/vue/VueAnnee.java
+++ JavaFX/src/vue/VueAnnee.java
@@ -1,79 +1,115 @@
 package vue;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import action.ControleurTemperature;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import modele.Temperature;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 
 public class VueAnnee extends Scene {
 	
-	protected GridPane grilleTemperature;
 	private ControleurTemperature controleur = null;
+
+	protected GridPane grilleTemperature;
 	private Label valeurMois, valeurTemperature;
-	private Button boutonJour, boutonMois;
-	
-	public VueAnnee() {
-		super(new GridPane(),400,400);
-		grilleTemperature = (GridPane)this.getRoot();
-		this.boutonMois = new Button("Mois");
-		this.boutonJour = new Button("Jour");
-		
-		this.grilleTemperature.getChildren().clear();
-		this.grilleTemperature.add(new Label("Mois"),0,0);
-		this.grilleTemperature.add(new Label("Temperature(°C)"),1,0);
-		
-		int position = 1;
-		this.boutonJour.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				controleur.notifierNaviguerVueJour();
-			}
-		});
-		this.boutonMois.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				controleur.notifierNaviguerVueMois();
-			}
-		});
-		this.grilleTemperature.add(this.boutonJour, 1, ++position);
-		this.grilleTemperature.add(this.boutonMois, 2, ++position);
-	}
-	
-	public void afficherListeEquipes(List<Temperature> listeTemperatures) {
-		/*this.grilleTemperature.getChildren().clear();
-		this.grilleTemperature.add(new Label("Mois"),0,0);
-		this.grilleTemperature.add(new Label("Temperature(°C)"),1,0);*/
-		//this.grilleTemperature.add(new Label(""), 4, 0);
-		int position=1;
-		for(Temperature temperature:listeTemperatures) {
-			position++;
-			this.grilleTemperature.add(new Label(""+temperature.getDate().getMonth()), 0, position);
-			this.grilleTemperature.add(new Label(""+temperature.getTemperature()), 1, position);
-		}
-		/*this.boutonJour.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				controleur.notifierNaviguerVueJour();
-			}
-		});
-		this.boutonMois.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				controleur.notifierNaviguerVueMois();
-			}
-		});
-		this.grilleTemperature.add(this.boutonJour, 1, ++position);
-		this.grilleTemperature.add(this.boutonMois, 2, ++position);*/
-	}
+	private Button retour;
 	
 	public void setControleur(ControleurTemperature controleur) {
 		this.controleur = controleur;
 	}
+	
+	public VueAnnee() {
+		super(new GridPane(),400,400);
+		System.out.println("VueAnnee : VueAnnee()");
+		
+		grilleTemperature = (GridPane)this.getRoot();
+		
+		this.retour = new Button("Retour");
+		this.retour.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				controleur.notifierNaviguerVueAccueil();
+			}
+		});
+		valeurMeteo(urlXml("http://167.114.152.43/PHP/2018/"));
+		
+		this.grilleTemperature.getChildren().clear();
+		this.grilleTemperature.add(new Label("Mois"),0,0);
+		this.grilleTemperature.add(new Label("Temperature(°C)"),0,1);
+		this.grilleTemperature.add(this.retour, 0, 2);
+	}
+	
+	
+	public String urlXml(String urlXml) {
+		String xml = "";
+		
+		try {
+			   URL url = new URL(urlXml);
+			   HttpURLConnection request1 = (HttpURLConnection) url.openConnection();
+			   request1.setRequestMethod("GET");
+			   		
+			   //String code = String.valueOf(request1.getResponseCode());
+			   //System.out.println("Error code "+code);
+			   
+			   InputStream is = request1.getInputStream();
+
+			   BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+			   String line;
+			   while ((line = bufferedReader.readLine()) != null) {
+				   xml += line + "\n";
+			   }
+			   //System.out.println(fileXml);
+			   
+			   return xml;
+			   
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    return "erreur lecture";
+		}
+	}
+	
+	
+	private void valeurMeteo(String xml) {
+		ArrayList<String> meteo = new ArrayList<>();
+		String annee;
+		ArrayList<String> mois = new ArrayList<>();
+		ArrayList<String> temperature = new ArrayList<>();
+		String max;
+		String min;
+		String moyenne;
+		
+		try {
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			InputSource src = new InputSource();
+			src.setCharacterStream(new StringReader(xml));
+
+			Document doc = builder.parse(src);
+			
+			annee = doc.getElementsByTagName("annee").item(0).getTextContent();
+			max = doc.getElementsByTagName("max").item(0).getTextContent();
+			
+			System.out.println(annee);
+			System.out.println(max);
+			
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+	}
+
 }
